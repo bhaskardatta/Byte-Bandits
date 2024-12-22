b110b73d70b01989b0aec12f26225dd15a27338d
NandeeshaHK <nandeeshahk875@gmail.com>
2024-12-22 14:07:17 +0530

    Integrate all the sequences of creating the audio tones, run with the url as argument, and wait for it to execute in sequence

--- integrate.sh
+++ integrate.sh
@@ -6,7 +6,6 @@ SCRIPT1="youtube.py"
 SCRIPT2="extract_pod.py"
 SCRIPT3="audio_concatenator.py"
 
-# Check if the correct number of arguments is provided for the first script
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <link_argument>"
   exit 1
@@ -14,11 +13,9 @@ fi
 
 LINK_ARG="$1"
 
-# Run the first script with the link argument
 echo "Running $SCRIPT1 with argument: $LINK_ARG..."
 python3 "$SCRIPT1" "$LINK_ARG"
 
-# Check if the first script exited with an error
 if [ $? -ne 0 ]; then
   echo "Error: $SCRIPT1 failed. Exiting."
   exit 1
@@ -26,17 +23,14 @@ fi
 
 echo "$SCRIPT1 executed successfully."
 
-# Check if the specified folder is empty
 if [ -z "$(ls -A "$FOLDER_PATH")" ]; then
   echo "Error: Folder $FOLDER_PATH is empty. Exiting."
   exit 1
 fi
 
-# Run the second script
 echo "Running $SCRIPT2..."
 python3 "$SCRIPT2"
 
-# Check if the second script exited with an error
 if [ $? -ne 0 ]; then
   echo "Error: $SCRIPT2 failed. Exiting."
   exit 1
@@ -44,11 +38,9 @@ fi
 
 echo "$SCRIPT2 executed successfully."
 
-# Run the third script
 echo "Running $SCRIPT3..."
 python3 "$SCRIPT3"
 
-# Check if the third script exited with an error
 if [ $? -ne 0 ]; then
   echo "Error: $SCRIPT3 failed. Exiting."
   exit 1
@@ -56,11 +48,9 @@ fi
 
 echo "$SCRIPT3 executed successfully."
 
-# Empty the specified folder
 echo "Emptying folder $FOLDER_PATH..."
 rm -rf "$FOLDER_PATH"/*
 
-# Check if folder was emptied successfully
 if [ $? -ne 0 ]; then
   echo "Error: Failed to empty folder $FOLDER_PATH."
   exit 1
